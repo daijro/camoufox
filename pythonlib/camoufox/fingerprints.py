4eb559f043e81a4fe61c4353479dca3323b3d0f2
daijro <daijro.dev@gmail.com>
2024-09-19 16:15:14 -0500

    pythonlib: Fix out of bounds screen values 0.1.0b4

--- pythonlib/camoufox/fingerprints.py
+++ pythonlib/camoufox/fingerprints.py
@@ -5,13 +5,13 @@ from browserforge.fingerprints import Fingerprint, FingerprintGenerator
 from yaml import CLoader, load
 
 # Load the browserforge.yaml file
-with open(os.path.join(os.path.dirname(__file__), 'browserforge.yaml'), 'r') as f:
+with open(os.path.join(os.path.dirname(__file__), 'browserforge.yml'), 'r') as f:
     BROWSERFORGE_DATA = load(f, Loader=CLoader)
 
 FP_GENERATOR = FingerprintGenerator(browser='firefox', os=('linux', 'macos', 'windows'))
 
 
-def _cast_to_properties(camoufox_data, cast_enum: dict, bf_dict: dict):
+def _cast_to_properties(camoufox_data: dict, cast_enum: dict, bf_dict: dict) -> None:
     """
     Casts Browserforge fingerprints to Camoufox config properties.
     """
@@ -26,8 +26,11 @@ def _cast_to_properties(camoufox_data, cast_enum: dict, bf_dict: dict):
         # If the value is a dictionary, recursively recall
         if isinstance(data, dict):
             _cast_to_properties(camoufox_data, type_key, data)
-        else:
-            camoufox_data[type_key] = data
+            continue
+        # Fix values that are out of bounds
+        if type_key.startswith("screen.") and isinstance(data, int) and data < 0:
+            data = 0
+        camoufox_data[type_key] = data
 
 
 def from_browserforge(fingerprint: Fingerprint) -> dict:
