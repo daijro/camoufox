4b41794f1cfabf04f71e8e7ca8d490bf67a8dd46
daijro <daijro.dev@gmail.com>
2024-12-03 22:50:50 -0600

    pythonlib: Support for main world evaluation 0.4.5

--- pythonlib/camoufox/__version__.py
+++ pythonlib/camoufox/__version__.py
@@ -8,7 +8,7 @@ class CONSTRAINTS:
     The minimum and maximum supported versions of the Camoufox browser.
     """
 
-    MIN_VERSION = 'beta.17'
+    MIN_VERSION = 'beta.18'
     MAX_VERSION = '1'
 
     @staticmethod
