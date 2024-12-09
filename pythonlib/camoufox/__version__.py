8c17edf2ccb0c0e866a0fcb2f98dbf8f2d6d0300
daijro <daijro.dev@gmail.com>
2024-12-09 08:26:30 -0600

    pythonlib: Auto offset Canvas anti-aliasing

--- pythonlib/camoufox/__version__.py
+++ pythonlib/camoufox/__version__.py
@@ -8,7 +8,7 @@ class CONSTRAINTS:
     The minimum and maximum supported versions of the Camoufox browser.
     """
 
-    MIN_VERSION = 'beta.18'
+    MIN_VERSION = 'beta.19'
     MAX_VERSION = '1'
 
     @staticmethod
